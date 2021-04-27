8b59e0c8dfa6e73654adb7a111d318234c47cfc5
saiprasad137 <saiprasad14350@gmail.com>
2021-04-27 16:16:21 +0530

    added welcome component

--- connect/src/components/welcome.js
+++ connect/src/components/welcome.js
@@ -1,1 +1,14 @@
-import React from 'react'
+import React, { Component } from 'react'
+
+class welcome extends Component {
+    render() {
+        return (
+            <div>
+                hello
+            </div>
+        )
+    }
+}
+
+
+export default welcome
