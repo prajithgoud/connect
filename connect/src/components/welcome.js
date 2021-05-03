ebd86965196226a2446f46899fdd2e51f1ff45a0
roshan4545 <60842890+roshan4545@users.noreply.github.com>
2021-05-03 14:27:50 +0530

    Update welcome.js

--- connect/src/components/welcome.js
+++ connect/src/components/welcome.js
@@ -1,1 +1,20 @@
-import React from 'react'
+import React, { Component } from 'react';
+import "./welcome.css";
+
+
+export default class Welcome extends Component {
+
+    render(){
+        return(
+            <div>
+            <div className="landing-page">
+
+            </div>
+            <div className="landing-text">
+              <h1>Connect</h1>
+              <p>A Social Media Website For OU Students.</p>
+            </div>
+            </div>
+        )
+    }
+}
