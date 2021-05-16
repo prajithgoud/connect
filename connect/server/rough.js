b0885de53e233639e697884de6f9b2a16c7bedb6
saiprasad137 <saiprasad14350@gmail.com>
2021-05-16 18:36:54 +0530

    Just added CSS dependencies

---
+++ connect/server/rough.js
@@ -0,0 +1,11 @@
+console.log(Math.floor(Math.random() * (1000000 - 100000) + 100000));
+
+// function between(min, max) {  
+//     return Math.floor(
+//       Math.random() * (max - min) + min
+//     )
+//   }
+  
+//   console.log(  
+//     between(100000,1000000)
+//   )
--- connect/src/components/header.js
+++ connect/src/components/header.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import axios from 'axios';
 import Welcome from "./welcome"
+// import { link } from "node:fs";
+import "./destination/style.css";
+
 export default class Header extends Component {
 
   constructor(props) {
@@ -47,25 +50,68 @@ onSubmit(e) {
 
   render() {
     return (
-      <nav class="navbar navbar-expand-lg navbar-dark fixed-top bg-dark">
-  <div class="container-fluid">
+      <nav class="navbar navbar-expand-lg flex-shrink-0  navbar-dark fixed-top bg-dark">
+  <div class="container-fluid flex-shrink-0 ">
     <a href="/" class="navbar-brand">Connect</a>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-      <li class="nav-item">
+      {/* <ul class="navbar-nav me-auto mb-2 mb-lg-0"> */}
+      <ul class = "w-full block flex-grow sm:flex text-white sm:items-center sm:w-auto">
+      {/* <li class="nav-item"> */}
+      <li class = "text-sm sm:flex">
           <a class="nav-link" aria-current="page" href="/signin">SignUp</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="/login">login</a>
-        </li>
-      </ul>
-      </div>
-    <form class="d-flex" onSubmit= {this.onSubmit}>
-      <input class="form-control me-2" value={this.state.department} onChange={this.onChangedepartment} type="search" placeholder="Search" aria-label="Search" />
-      <button class="btn btn-outline-success" type="submit">Search</button>
-    </form>
-  </div>
-</nav>
+              {/* <a
+              href="#"
+              class="block mt-1 sm:inline-block sm:mt-0 text-white-700 hover:text-white mr-4"
+              >
+              Your Business
+              </a> */}
+         </li>
+       </ul>
+       </div>
+     <form class="d-flex" onSubmit= {this.onSubmit}>
+       <input class="form-control me-2" value={this.state.department} onChange={this.onChangedepartment} type="search" placeholder="Search" aria-label="Search" />
+       <button class="btn btn-outline-success" type="submit">Search</button>
+     </form>
+   </div>
+ </nav>
+      //   <nav class="flex items-center justify-between flex-wrap bg-gray-800 p-2">
+      //   <div class="flex items-center flex-shrink-0 text-white mr-6">
+      //     <span class="font-bold text-xl">Biznet</span>
+      //   </div>
+  
+      //   <div class="w-full block flex-grow sm:flex text-white sm:items-center sm:w-auto">
+      //     <div class="text-sm sm:flex-grow">
+      //       <a
+      //         href="#"
+      //         class="block mt-4 sm:inline-block sm:mt-0 text-teal-700 hover:text-white mr-4"
+      //       >
+      //         Your Business
+      //       </a>
+      //       <a
+      //         href="#"
+      //         class="block mt-4 sm:inline-block sm:mt-0 text-teal-200 hover:text-white mr-4"
+      //       >
+      //         Networking
+      //       </a>
+      //       <a
+      //         href="#"
+      //         class="block mt-4 sm:inline-block sm:mt-0 text-teal-200 hover:text-white"
+      //       >
+      //         Blog
+      //       </a>
+      //     </div>
+      //     <div>
+      //       <a
+      //         href="#"
+      //         class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-blue-500 hover:bg-white mt-4 md:mt-0"
+      //         >Login</a
+      //       >
+      //     </div>
+      //   </div>
+      // </nav> 
     );
   }
 }
